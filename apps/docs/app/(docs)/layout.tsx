1a9306adcc29e2869847f003067979ea4aadc9cd
ridhoassuryadi <ulunmuhammadridho@gmail.com>
2025-06-08 05:08:50 +0700

    ci(app[): move from next into elixir

--- apps/docs/app/(docs)/layout.tsx
+++
@@ -1,8 +0,0 @@
-import { DocsLayout } from "fumadocs-ui/layouts/docs"
-import type { ReactNode } from "react"
-import { docsOptions } from "@/app/layout.config"
-import "fumadocs-twoslash/twoslash.css"
-
-export default function Layout({ children }: { children: ReactNode }) {
-  return <DocsLayout {...docsOptions}>{children}</DocsLayout>
-}
