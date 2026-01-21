fc86069135a576651b4a4836733276f5081ee465
ridhoassuryadi <ulunmuhammadridho@gmail.com>
2026-01-21 19:30:26 +0700

    chore: update pnpm-lock.yaml

--- apps/web/app/[orgId]/page.tsx
+++
@@ -1,5 +0,0 @@
-import { redirect } from 'next/navigation';
-
-export default function OrgIdPage() {
-   redirect('treonstudio/team/CORE/all');
-}
--- apps/web/app/page.tsx
+++
@@ -1,5 +0,0 @@
-import { redirect } from 'next/navigation';
-
-export default function Home() {
-   redirect('treonstudio/team/CORE/all');
-}
