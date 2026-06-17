55deaa751dae4274ebb4768ac6a01836cadccc05
ridhoassuryadi <ulunmuhammadridho@gmail.com>
2026-06-17 19:43:03 +0700

    chore(cli): bump Version to 4.0.0
    Major version bump. Reflects the hard break (deleted dead code, module
    path migration to /v4). Reported by 'radas version' and compared
    against GitHub releases by internal/updater.
    
    Part of the 4.0.0 cleanup.

--- apps/cli/constants/version.go
+++ apps/cli/constants/version.go
@@ -3,9 +3,9 @@ package constants
 // Version information
 const (
 	// Version is the current version of the application
-	Version = "0.2.0"
-	
+	Version = "4.0.0"
+
 	// VersionCheckURL is the URL to check for new versions
 	// This should point to a released version JSON file on GitHub
 	VersionCheckURL = "https://api.github.com/repos/raizora/radas/releases/latest"
-) 
+)
