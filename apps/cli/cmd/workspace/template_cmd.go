18249a293830d37e79e5ffbcb598a26c5a391a7e
ridhoassuryadi <ulunmuhammadridho@gmail.com>
2026-06-20 05:40:01 +0700

    feat(workspace): add template add command for remote template installation

--- apps/cli/cmd/workspace/template_cmd.go
+++ apps/cli/cmd/workspace/template_cmd.go
@@ -2,6 +2,8 @@ package workspace
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
 
 	"github.com/raizora/radas/v4/internal/generator"
 	"github.com/spf13/cobra"
@@ -40,6 +42,76 @@ var templateListCmd = &cobra.Command{
 	},
 }
 
+var templateAddCmd = &cobra.Command{
+	Use:   "add <git-url>",
+	Short: "Add a template from a git repository",
+	Args:  cobra.ExactArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		remoteURL := args[0]
+		targetDir := "./templates"
+
+		reg := &generator.Registry{}
+		tmpl, err := reg.Add(remoteURL, targetDir)
+		if err != nil {
+			return fmt.Errorf("add template: %w", err)
+		}
+
+		cmd.Printf("Added template %q from %s\n", tmpl.Name, remoteURL)
+		cmd.Printf("  Location: %s\n", tmpl.Dir)
+		return nil
+	},
+}
+
+var templateCreateCmd = &cobra.Command{
+	Use:   "create <name>",
+	Short: "Create a new local template scaffold",
+	Args:  cobra.ExactArgs(1),
+	Long: `Creates a new template directory under ./templates/ with a
+template.yml definition file and a sample .gotpl file.`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		name := args[0]
+		targetDir := filepath.Join(".", "templates", name)
+
+		if err := os.MkdirAll(targetDir, 0755); err != nil {
+			return fmt.Errorf("create template dir: %w", err)
+		}
+
+		ymlContent := fmt.Sprintf(`name: %s
+description: A %s template
+version: 1
+variables:
+  - name: name
+    description: Name of the thing
+    prompt: What is the name?
+    default: my-thing
+outputs:
+  - template: main.gotpl
+    target: "{{.name}}.md"
+`, name, name)
+
+		ymlPath := filepath.Join(targetDir, "template.yml")
+		if err := os.WriteFile(ymlPath, []byte(ymlContent), 0644); err != nil {
+			return fmt.Errorf("write template.yml: %w", err)
+		}
+
+		gotplContent := fmt.Sprintf(`# {{.name}}
+
+Generated from the %s template.
+`, name)
+
+		gotplPath := filepath.Join(targetDir, "main.gotpl")
+		if err := os.WriteFile(gotplPath, []byte(gotplContent), 0644); err != nil {
+			return fmt.Errorf("write main.gotpl: %w", err)
+		}
+
+		cmd.Printf("Created template %q at %s\n", name, targetDir)
+		cmd.Println("  Edit template.yml to configure variables and outputs")
+		cmd.Println("  Edit .gotpl files to customize template content")
+		return nil
+	},
+}
+
 func init() {
 	templateCmd.AddCommand(templateListCmd)
+	templateCmd.AddCommand(templateAddCmd)
 }
